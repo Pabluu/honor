2d57447706d4a8af3b768485ca27725a7af0f09c
Pablo Henrique SC <pabloph0077@gmail.com>
2023-08-26 18:04:16 -0300

    chore: change the look

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,14 +1,25 @@
+import { Coins } from 'phosphor-react'
 import { FormUser } from '../../components/FormUser'
 import { SignContainer } from '../../components/Sign/styles'
-import { Title, UpContainer } from './styles'
+import { DescriptionSignUp, UpContainer } from './styles'
 
 export function SignUp() {
   return (
     <SignContainer>
       <UpContainer>
-        <Title>Cadastro</Title>
+        {/* <Title>Cadastro</Title> */}
         <FormUser />
       </UpContainer>
+
+      <DescriptionSignUp>
+        <Coins size={64} />
+
+        <h1>Honor Money</h1>
+
+        <p>Já possui uma conta ?</p>
+
+        <button>Login</button>
+      </DescriptionSignUp>
     </SignContainer>
   )
 }
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,13 +1,14 @@
 import { styled } from 'styled-components'
 
 export const UpContainer = styled.div`
-  width: 100%;
+  width: 23rem;
   height: 100%;
 
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  gap: 1.25rem;
+  gap: 1.2rem;
+  padding: 1.5rem;
 `
 
 export const Title = styled.h1`
@@ -17,5 +18,51 @@ export const Title = styled.h1`
   font: 700 1.5rem Roboto;
 
   border-bottom: 1px solid ${(props) => props.theme['gray-700']};
-  height: 2.65rem;
+  height: 2.5rem;
+`
+
+export const DescriptionSignUp = styled.div`
+  width: 23rem;
+  height: 100%;
+
+  padding: 1rem;
+
+  background-color: ${(props) => props.theme['blue-700']};
+  color: white;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 0.75rem;
+
+  &::selection {
+    background-color: white;
+    color: ${(props) => props.theme['blue-700']};
+  }
+
+  h1 {
+    color: white;
+    font-weight: 700;
+    font-size: 1.5rem;
+    text-transform: uppercase;
+  }
+
+  p {
+    color: white;
+  }
+
+  button {
+    width: 50%;
+    background-color: white;
+    color: ${(props) => props.theme['blue-700']};
+
+    border: 1px solid ${(props) => props.theme['blue-700']};
+    border-radius: 5px;
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.85);
+      /* color: white; */
+    }
+  }
 `
