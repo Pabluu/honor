21648aaa64b9e1fb3a94837513aa007082e2844a
Pablo Henrique SC <pabloph0077@gmail.com>
2023-08-28 10:39:05 -0300

    feat: add new Route

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,10 @@ import { SignUp } from './pages/SignUp'
 export function Router() {
   return (
     <Routes>
+      <Route
+        path="/"
+        element={<h1 style={{ color: 'black' }}>PAGINA INICIAL</h1>}
+      />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
   )
