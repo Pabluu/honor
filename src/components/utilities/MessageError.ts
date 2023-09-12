d9fa2c380b833f4347692fc9a39233333a716a19
Pablo Henrique SC <pabloph0077@gmail.com>
2023-09-12 15:28:45 -0300

    feat: create component to errors

---
+++ src/components/utilities/MessageError.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const MessageError = styled.span`
+  color: ${(props) => props.theme['red-600']};
+  font-weight: bold;
+  transition: all 1s;
+  min-height: 19px;
+  margin-top: 5px;
+  font-size: 0.85rem;
+`
