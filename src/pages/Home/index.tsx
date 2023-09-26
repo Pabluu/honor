3a4d9a00c7fb8b028261554cd3fbab29f910aa11
Pablo Henrique SC <pabloph0077@gmail.com>
2023-09-26 16:49:35 -0300

    feat: initial screen home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from '../../components/Navbar'
+import { HomeContainer } from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <NavBar />
+    </HomeContainer>
+  )
+}
